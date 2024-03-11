ca5baa43c95db115d4217b4eb515b9f5357595a7
Salim BoBO <salimtuxtatoshov19@gmail.com>
2024-03-11 22:50:50 +0500

    Initial commit

---
+++ my_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world.")
+def about(request):
+    return HttpResponse("About me")
+
+def hello(request, first_name):
+    return HttpResponse(f"Hello {first_name}")
+def add(request,num1,num2):
+    return HttpResponse(f"{num1} + {num2}={num1+num2}")
